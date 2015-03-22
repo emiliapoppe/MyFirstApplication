50a986e6a08b666b1ea26d830355f3059e7100c8
emiliapoppe <emiliapoppe@gmail.com>
2015-03-21 18:14:02 -0700

    init
    learning git!

---
+++ app/src/main/java/com/first_application/emilia/myfirstapplication/Text_Button.java
@@ -0,0 +1,47 @@
+package com.first_application.emilia.myfirstapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class Text_Button extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text__button);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_text__button, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSubmit(View view) {
+        //27:43
+        Toast toast = Toast.makeText(this,"Hello World",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
